318fbff4eccf3cc518807798a9d48f49350eba8b
chinmay saraswat <226301072@gkv.ac.in>
2025-12-02 11:33:30 +0530

    changes in student

--- models/Student.js
+++ models/Student.js
@@ -10,34 +10,35 @@ const studentSchema = new mongoose.Schema({
     required: true,
     validate: {
       validator: (v) => /^[0-9]{10}$/.test(v),
-      message: "Contact number must be exactly 10 digits"
-    }
+      message: "Contact number must be exactly 10 digits",
+    },
   },
-  collegeName: { type: String, required: true },
-  district:{type: String, required: true},
-  city:{type: String, required: true},
-  state:{type:String,required:true},
   id: {
     type: String,
     unique: true,
+    sparse: true,
+    required: false,
+    default: undefined,
   },
 
+  collegeName: { type: String, required: true },
+  district: { type: String, required: true },
+  city: { type: String, required: true },
+  state: { type: String, required: true },
+
   courseName: { type: String, required: true },
-  
+
   branch: { type: String, required: true },
-  year:{type :String, required:true},
+  year: { type: String, required: true },
   passoutYear: { type: Number, required: true },
 
   studentCode: { type: String, unique: true },
-  createdAt: { type: Date, default: Date.now }
+  createdAt: { type: Date, default: Date.now },
 });
-
 
 // 📌 Simple Unique Code Logic
 studentSchema.pre("save", async function (next) {
-
   if (!this.studentCode) {
-
     // Count total students and add +1
     const count = await mongoose.model("Student").countDocuments();
 
@@ -45,7 +46,13 @@ studentSchema.pre("save", async function (next) {
     this.studentCode = `JOBFAIR-${String(count + 1).padStart(5, "0")}`;
   }
 
-  next;
+  next();
+});
+studentSchema.pre("save", function (next) {
+  if (this.id === "" || !this.id) {
+    this.id = { studentCode };
+  }
+  next();
 });
 
 module.exports = mongoose.model("Student", studentSchema);
